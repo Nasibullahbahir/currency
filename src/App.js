2b65a962648d2e63305a815aa8909462038632d6
Nasibullah Bahir <nasibullahbahir745gmail.com>
2025-07-21 18:01:03 +0430

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("USD");
+  const [toCur, setToCur] = useState("EUR");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function convert() {
+      if (fromCur === toCur) {
+        setConverted(amount);
+        return;
+      }
+
+      setIsLoading(true);
+
+      try {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[toCur]);
+      } catch (error) {
+        setConverted("Error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    convert();
+  }, [amount, fromCur, toCur]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <p>{isLoading ? "Loading..." : converted}</p>
+    </div>
+  );
+}
